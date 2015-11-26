70dc64342749bbef64a0ce5eb48c684d681a9202
Dhruv Mehra <dmehra6@gatech.edu>
2015-11-26 14:41:43 -0500

    Add team info animation

--- js/custom.js
+++ js/custom.js
@@ -18,15 +18,87 @@ $(document).ready(function() {
   var $designDescription = $('#team-description-design');
   var $businessDescription = $('#team-description-business');
 
-  var hideTeamDescription = function(argument) {
-    $businessDescription.fadeOut('fast');
-    $designDescription.fadeOut('fast');
-    $architectureDescription.fadeOut('fast');
-    $computingDescription.fadeOut('fast');
-    $mechanicalDescription.fadeOut('fast');
-    $businessDescription.fadeOut('fast');
-    $teamDescription.fadeOut('fast');
+  var hideTeamDescription = function() {
+    $businessDescription.toggle('fast');
+    $designDescription.toggle('fast');
+    $architectureDescription.toggle('fast');
+    $computingDescription.toggle('fast');
+    $mechanicalDescription.toggle('fast');
+    $electricalDescription.toggle('fast');
+    $teamDescription.toggle('fast');
+  };
+
+  var displayDescriptionElement = function(element) {
+    $teamContainer.fadeOut('fast');
+    $teamDescription.fadeIn('slow');
+    element.fadeIn('slow');
   };
 
   hideTeamDescription();
+
+  // Electrical
+  $electrical.click(function() {
+    $teamContainer.toggle('slow');
+    displayDescriptionElement($electricalDescription);
+  });
+
+  $electricalDescription.click(function() {
+    $(this).toggle('slow');
+    $teamContainer.toggle('fast');
+  });
+
+  // Mechanical
+  $mechanical.click(function() {
+    $teamContainer.toggle('slow');
+    displayDescriptionElement($mechanicalDescription);
+  });
+
+  $mechanicalDescription.click(function() {
+    $(this).toggle('slow');
+    $teamContainer.toggle('fast');
+  });
+
+  // Computing
+  $computing.click(function() {
+    $teamContainer.toggle('slow');
+    displayDescriptionElement($computingDescription);
+  });
+
+  $computingDescription.click(function() {
+    $(this).toggle('slow');
+    $teamContainer.toggle('fast');
+  });
+
+  // Architecture
+  $architecture.click(function() {
+    $teamContainer.toggle('slow');
+    displayDescriptionElement($architectureDescription);
+  });
+
+  $architectureDescription.click(function() {
+    $(this).toggle('slow');
+    $teamContainer.toggle('fast');
+  });
+
+  // Design
+  $design.click(function() {
+    $teamContainer.toggle('slow');
+    displayDescriptionElement($designDescription);
+  });
+
+  $designDescription.click(function() {
+    $(this).toggle('slow');
+    $teamContainer.toggle('fast');
+  });
+
+  // Business
+  $business.click(function() {
+    $teamContainer.toggle('slow');
+    displayDescriptionElement($businessDescription);
+  });
+
+  $businessDescription.click(function() {
+    $(this).toggle('slow');
+    $teamContainer.toggle('fast');
+  });
 });
